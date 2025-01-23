3667f6b5f3952bc095842fdc2b0b184002ce6650
federicascognamiglio <scognamigliofede99@gmail.com>
2025-01-23 14:41:14 +0100

    feat: index route completed

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,12 +2,19 @@ const connection = require("../data/movies_db");
 
 // Index
 const index = (req, res) => {
-    res.json({message: "All movies"})
+    const sql = "SELECT * FROM `movies`"
+    connection.query(sql, (err, movies) => {
+        if (err) return res.status(500).json({ status: "fail", error: err.message })
+        res.status(200).json({
+            status: "success",
+            data: movies
+        })
+    })
 }
 
 // Show
 const show = (req, res) => {
-    res.json({message: "Show movie"})
+    res.json({ message: "Show movie" })
 }
 
 module.exports = {
